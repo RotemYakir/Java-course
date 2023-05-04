6cbc0ed7939b2bdd388a7a07d79a6c8924294dd0
ryaki <ryaki@LAPTOP-ESGLLBM2.lan>
2023-05-04 19:54:09 +0300

    concurrency

--- 2023-05-04 concurrency/src/scheduledTasksDemos/Demo2.java
+++ 2023-05-04 concurrency/src/scheduledTasksDemos/Demo2.java
@@ -2,7 +2,6 @@ package scheduledTasksDemos;
 
 import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
-import java.util.concurrent.ScheduledFuture;
 import java.util.concurrent.TimeUnit;
 
 public class Demo2 {
